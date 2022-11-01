7b5543fa6f4df9ae0124348f2527e6d4f7c0f819
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-11-01 18:07:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,13 +75,13 @@ console.log("argv:", argv);
 
 //! Шаг 5 ==> Запусти команды в терминале и сделай отдельный скриншот результата выполнения каждой команды.
 //? Получаем и выводим весь список контактов в виде таблицы(console.table)
-//* node index.js--action list
+//* node index.js --action list
 
 //? Получаем контакт по id
-//* node index.js--action get--id 5
+//* node index.js --action get --id 5
 
 //? Добавялем контакт
-//* node index.js--action add--name Mango--email mango @gmail.com--phone 322 - 22 - 22
+//* node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
 
 //? Удаляем контакт
-//* node index.js--action remove--id = 3
+//* node index.js --action remove--id = 3
